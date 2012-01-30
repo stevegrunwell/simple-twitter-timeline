e9ae3473be5f1711e35ced20e8b98780527a4567
Steve Grunwell <stevegrunwell@gmail.com>
2012-01-29 23:35:14 -0500

    Initial commit

---
+++ tests/twitterfeed_test.php
@@ -0,0 +1,261 @@
+<?php
+/** Tests for the SimpleTwitterTimeline class */
+
+require_once 'simpletest/autorun.php';
+require_once '../twitter.class.php';
+
+/** Set the user to run tests against */
+define('DEFAULT_TWITTER_USER', 'stevegrunwell');
+
+class TestOfTwitterClass extends UnitTestCase {
+
+  function testClassInstantiation(){
+    // Instantiate without/invalid username to make sure we're calling the set_username() method
+    $this->expectError('Invalid username');
+    new SimpleTwitterTimeline();
+
+    // Ensure that options are being saved by passing (non-default) options
+    $args = array(
+      'exclude_replies' => true,
+      'limit' => 15,
+      'parse_links' => false,
+      'link_class' => 'parsed-link-test'
+    );
+    $twitter = new SimpleTwitterTimeline(DEFAULT_TWITTER_USER, $args);
+    foreach( $args as $k=>$v ){
+      $this->assertEqual($twitter->get_opt($k), $args[$k]);
+    }
+  }
+
+  function testFunctionSetOpt(){
+    $twitter = new SimpleTwitterTimeline(DEFAULT_TWITTER_USER);
+    $this->assertFalse(isset($twitter->opts['foobar']));
+    $twitter->set_opt('foobar', 'baz');
+    $this->assertTrue(isset($twitter->opts['foobar']));
+    $this->assertEqual($twitter->opts['foobar'], 'baz');
+    $this->assertIsA($twitter->opts['foobar'], 'string');
+    $twitter->set_opt('foobar', array('baz'));
+    $this->assertTrue(isset($twitter->opts['foobar']));
+    $this->assertNotEqual($twitter->opts['foobar'], 'baz');
+    $this->assertEqual($twitter->opts['foobar'], array('baz'));
+    $this->assertIsA($twitter->opts['foobar'], 'array');
+    $twitter->set_opt('foobar', new StdClass);
+    $this->assertIsA($twitter->opts['foobar'], 'stdClass');
+  }
+
+  function testFunctionGetOpt(){
+    $twitter = new SimpleTwitterTimeline(DEFAULT_TWITTER_USER);
+    $twitter->set_opt('foobar', 'baz');
+    $this->assertEqual($twitter->get_opt('foobar'), $twitter->opts['foobar']);
+    $this->assertFalse(isset($twitter->opts['baz']));
+    $this->assertFalse($twitter->get_opt('baz'));
+    $this->assertTrue($twitter->get_opt('baz', true));
+    $this->assertEqual($twitter->get_opt('baz', 'foo'), 'foo');
+  }
+
+  function testFunctionSetUsername(){
+    $twitter = new SimpleTwitterTimeline(DEFAULT_TWITTER_USER);
+
+    // Pass a valid username
+    $this->assertTrue($twitter->set_username(DEFAULT_TWITTER_USER));
+    $this->assertTrue($twitter->set_username('abc123'));
+
+    // Pass a non-string
+    $this->expectError('SimpleTwitterTimeline does not accept user IDs (yet)');
+    $this->assertFalse($twitter->set_username(123));
+    $this->expectError('SimpleTwitterTimeline does not accept user IDs (yet)');
+    $this->assertFalse($twitter->set_username(true));
+    $this->expectError('Invalid username');
+    $this->assertFalse($twitter->set_username(array()));
+    $this->expectError('Invalid username');
+    $this->assertFalse($twitter->set_username(new stdClass));
+  }
+
+  function testFunctionGetUsername(){
+    $twitter = new SimpleTwitterTimeline(DEFAULT_TWITTER_USER);
+    $this->assertIsA($twitter->get_username(), 'string');
+    $this->assertEqual($twitter->get_username(), DEFAULT_TWITTER_USER);
+    $this->assertNotEqual($twitter->get_username(), ucwords(DEFAULT_TWITTER_USER));
+    $this->assertNotEqual($twitter->get_username(), 'SteveJobs');
+    $twitter->set_username('SteveJobs');
+    $this->assertEqual($twitter->get_username(), 'SteveJobs');
+  }
+
+  function testFunctionGetProfileUrl(){
+    $twitter = new SimpleTwitterTimeline(DEFAULT_TWITTER_USER);
+    $this->assertIsA($twitter->get_profile_url(), 'string');
+    $this->assertIdentical($twitter->get_profile_url(), 'https://twitter.com/#!/' . DEFAULT_TWITTER_USER);
+    $this->assertTrue(filter_var($twitter->get_profile_url(), FILTER_VALIDATE_URL));
+  }
+
+  function testFunctionGetProfileLink(){
+    $twitter = new SimpleTwitterTimeline(DEFAULT_TWITTER_USER);
+    $this->assertIsA($twitter->get_profile_link(), 'string');
+    $this->assertEqual($twitter->get_profile_link(), '<a href="https://twitter.com/#!/' . DEFAULT_TWITTER_USER . '" title="Follow @' . DEFAULT_TWITTER_USER . ' on Twitter" rel="external">@' . DEFAULT_TWITTER_USER . '</a>');
+  }
+
+  function testFunctionCleanKey(){
+    $this->assertEqual(SimpleTwitterTimeline::clean_key('foobar'), 'foobar');
+    $this->assertEqual(SimpleTwitterTimeline::clean_key('foo-bar'), 'foo-bar');
+    $this->assertEqual(SimpleTwitterTimeline::clean_key('foo_bar'), 'foo_bar');
+    $this->assertEqual(SimpleTwitterTimeline::clean_key('foo1bar'), 'foo1bar');
+    $this->assertEqual(SimpleTwitterTimeline::clean_key('foo bar'), 'foobar');
+    $this->assertEqual(SimpleTwitterTimeline::clean_key('foo*bar'), 'foobar');
+    $this->assertEqual(SimpleTwitterTimeline::clean_key('foo$bar'), 'foobar');
+  }
+
+  function testCreateArray(){
+    $str = $str2 = 'foo, bar, baz';
+    $str2 .= ',';
+    $array = $array2 = array('foo', 'bar', 'baz');
+    $this->assertIsA(SimpleTwitterTimeline::create_array($str), 'array');
+    $this->assertEqual(SimpleTwitterTimeline::create_array($str), $array);
+    $this->assertEqual(SimpleTwitterTimeline::create_array($str2), $array);
+
+    // Array filtering and bypass
+    $this->assertEqual(SimpleTwitterTimeline::create_array($array), $array);
+    $this->assertIsA(SimpleTwitterTimeline::create_array($array), 'array');
+    $array2[] = '';
+    $this->assertEqual(SimpleTwitterTimeline::create_array($array2), $array);
+  }
+
+  function testFunctionGetTimeline(){
+    $opts = array(
+      'use_cache' => true,
+      'cache_filename' => 'tweetcache-test.json',
+      'cache_path' => dirname(__FILE__)
+    );
+    unlink(sprintf('%s/%s', $opts['cache_path'], $opts['cache_filename']));
+    $twitter = new SimpleTwitterTimeline(DEFAULT_TWITTER_USER, $opts);
+    $timeline = $twitter->get_public_timeline();
+    $cache = $twitter->get_timeline();
+    $this->assertEqual($timeline, $cache);
+    $this->assertIsA($cache, 'array');
+  }
+
+  function testFunctionGetPublicTimeline(){
+    $twitter = new SimpleTwitterTimeline(DEFAULT_TWITTER_USER);
+    $timeline = $twitter->get_public_timeline();
+    $this->assertIsA($timeline, 'array');
+    $this->assertTrue(isset($timeline['0']->text));
+  }
+
+  function testFunctionGetTweetCache(){
+    $opts = array(
+      'use_cache' => true,
+      'cache_filename' => 'tweetcache-test.json',
+      'cache_path' => dirname(__FILE__)
+    );
+    $twitter = new SimpleTwitterTimeline(DEFAULT_TWITTER_USER, $opts);
+    $this->assertIsA($twitter->get_tweet_cache(), 'array');
+  }
+
+  function testFunctionSaveTweetCache(){
+    $opts = array(
+      'use_cache' => true,
+      'cache_filename' => 'tweetcache-test.json',
+      'cache_path' => dirname(__FILE__)
+    );
+    $twitter = new SimpleTwitterTimeline(DEFAULT_TWITTER_USER, $opts);
+    $test_data = new stdClass();
+    $test_data->foo = 'bar';
+    $filename = sprintf('%s/%s', $twitter->get_opt('cache_path'), $twitter->get_opt('cache_filename'));
+    @unlink($filename);
+
+    $this->assertTrue($twitter->save_tweet_cache($test_data));
+    $this->assertTrue(file_exists($filename));
+    $this->assertTrue(filemtime($filename));
+    $this->assertIsA(filemtime($filename), 'integer');
+  }
+
+  function testFunctionCacheValid(){
+    $opts = array(
+      'use_cache' => true,
+      'cache_filename' => 'tweetcache-test.json',
+      'cache_path', dirname(__FILE__)
+    );
+    $twitter = new SimpleTwitterTimeline(DEFAULT_TWITTER_USER, $opts);
+    $this->assertIsA($twitter->cache_valid(), 'boolean');
+  }
+
+  function testFunctionParseLinks(){
+    $opts = array(
+      'parse_links' => true,
+      'link_class' => 'parsed-link'
+    );
+    $twitter = new SimpleTwitterTimeline(DEFAULT_TWITTER_USER, $opts);
+    $this->assertEqual($twitter->parse_links('This is an example link: http://www.example.com/'), 'This is an example link: <a href="http://www.example.com/" class="parsed-link">http://www.example.com/</a>');
+    $this->assertEqual($twitter->parse_links('This is an example link: https://www.example.com/'), 'This is an example link: <a href="https://www.example.com/" class="parsed-link">https://www.example.com/</a>');
+    $this->assertEqual($twitter->parse_links('This is an example link: http://www.example.com/foo/bar.jpg'), 'This is an example link: <a href="http://www.example.com/foo/bar.jpg" class="parsed-link">http://www.example.com/foo/bar.jpg</a>');
+    $this->assertEqual($twitter->parse_links('This is an example link: http://www.example.com/foo?bar=1'), 'This is an example link: <a href="http://www.example.com/foo?bar=1" class="parsed-link">http://www.example.com/foo?bar=1</a>');
+    $this->assertEqual($twitter->parse_links('This is an example link: http:// www.example.com/'), 'This is an example link: http:// www.example.com/');
+  }
+
+  /*function testFunctionProfanityFilter(){
+    $profane_tweets = array(
+      0 => 'Holy fuck on a fuck-stick!',
+      1 => 'Be good or Santa is going to kick your little ass!',
+      2 => 'Oh my God, they killed Kenny! You bastards!',
+      3 => 'Fuck this shit - http://fuckthisshit.com',
+      4 => 'Justin Bieber is fucking terrible',
+      5 => 'Eat shitake mushrooms!'
+    );
+    $nice_tweet = 'Good morning! I hope everyone has a wonderful day!';
+
+    // Test with the default profanity filter
+    $opts = array(
+      'profanity_filter' => true,
+    );
+    $twitter = new SimpleTwitterTimeline(DEFAULT_TWITTER_USER, $opts);
+    $this->assertIsA($twitter->profanity_filter($nice_tweet), 'string');
+    $this->assertEqual($twitter->profanity_filter($nice_tweet), $nice_tweet);
+    $this->assertIsA($twitter->profanity_filter($profane_tweets['0']), 'string');
+    $this->assertEqual($twitter->profanity_filter($profane_tweets['0']), 'Holy **** on a ****-stick!');
+    $this->assertEqual($twitter->profanity_filter($profane_tweets['1']), $profane_tweets['1']);
+    $this->assertEqual($twitter->profanity_filter($profane_tweets['2']), $profane_tweets['2']);
+    $this->assertEqual($twitter->profanity_filter($profane_tweets['3']), '**** this shit - http://fuckthisshit.com');
+    $this->assertEqual($twitter->profanity_filter($profane_tweets['4']), 'Justin Bieber is ****ing terrible');
+
+    // Skip over profane tweets
+    $opts['profanity_filter_method'] = 'skip';
+    $twitter = new SimpleTwitterTimeline(DEFAULT_TWITTER_USER, $opts);
+    $this->assertIsA($twitter->profanity_filter($nice_tweet), 'string');
+    $this->assertEqual($twitter->profanity_filter($nice_tweet), $nice_tweet);
+    $this->assertIsA($twitter->profanity_filter($profane_tweets['0']), 'bool');
+    $this->assertFalse($twitter->profanity_filter($profane_tweets['0']));
+    $this->assertEqual($twitter->profanity_filter($profane_tweets['1']), $profane_tweets['1']);
+    $this->assertEqual($twitter->profanity_filter($profane_tweets['2']), $profane_tweets['2']);
+    $this->assertFalse($twitter->profanity_filter($profane_tweets['3']));
+
+    // Add to the default list
+    $opts['profanity_blacklist'] = 'Bieber,Gaga';
+    $twitter = new SimpleTwitterTimeline(DEFAULT_TWITTER_USER, $opts);
+    $this->assertFalse($twitter->profanity_filter($profane_tweets['4']));
+
+    // Custom blacklist of words (note: we're still on "skip" mode!)
+    $opts['profanity_blacklist_overwrite'] = true;
+    $twitter = new SimpleTwitterTimeline(DEFAULT_TWITTER_USER, $opts);
+    $this->assertEqual($twitter->profanity_filter($profane_tweets['0']), $profane_tweets['0']);
+    $this->assertFalse($twitter->profanity_filter($profane_tweets['4']));
+
+    // Switch back to 'censor' mode
+    $opts['profanity_filter_method'] = 'censor';
+    $twitter = new SimpleTwitterTimeline(DEFAULT_TWITTER_USER, $opts);
+    $this->assertEqual($twitter->profanity_filter($profane_tweets['4']), 'Justin ****** is fucking terrible');
+
+    // Test our whitelist
+    $opts['profanity_blacklist_overwrite'] = false;
+    $opts['profanity_blacklist'] = 'shit'; // This should be a default but we'll define it just to be sure
+    $opts['profanity_whitelist'] = 'shitake'; // Again, should be default
+    $twitter = new SimpleTwitterTimeline(DEFAULT_TWITTER_USER, $opts);
+    $this->assertEqual($twitter->profanity_filter($profane_tweets['5']), $profane_tweets['5']);
+
+    // Whitelist overwrites blacklist
+    $opts['profanity_blacklist'] = 'bastard';
+    $opts['profanity_whitelist'] = 'bastard';
+    $twitter = new SimpleTwitterTimeline(DEFAULT_TWITTER_USER, $opts);
+    $this->assertEqual($twitter->profanity_filter($profane_tweets['2']), $profane_tweets['2']);
+  }*/
+}
+
+?>
---
+++ twitter.class.php
@@ -0,0 +1,292 @@
+<?php
+/**
+ * Twitter parsing class
+ * @var str $username Twitter handle
+ * @var public array $opts - Configuration options
+ * @author Steve Grunwell <stevegrunwell@gmail.com>
+ */
+
+class SimpleTwitterTimeline {
+
+  public $username;
+  public $opts;
+
+  /**
+   * Class constructor
+   *
+   * Available options for $opts:
+   * exclude_replies: (bool) Don't count tweets that are replies to other tweets (according to Twitter) (default: false)
+   * limit: (int) The maximum number of tweets to return (default: 5)
+   *
+   * Tweet filters:
+   * parse_links: (bool) Wrap links within the tweets within <a> tags (default: true)
+   * link_class: (str) A class to apply to any parsed links (default: "parsed-link")
+   * profanity_filter: (bool) Use the built-in profanity filter?
+   * profanity_filter_method: (str) Censor the profanity or skip the tweet entirely (censor|skip, default: censor)
+   * profanity_blacklist: (str|array) Additional words to mark as profane (beyond the system defaults, default: null)
+   * profanity_blacklist_overwrite: (bool) Overwrite the default blacklist or append to it (default: false)
+   * profanity_whitelist: (str|array) Words that contain blacklisted words but should remain uncensored (example: "shitake"; default: null)
+   *
+   * Caching options:
+   * use_cache: (bool) Use timeline caching? (default: false)
+   * cache_filename: (str) The filename to use for the cache (default: tweets-{username}.json
+   * cache_path: (str) Where to store the cache file (default: dirname(__FILE__))
+   *
+   * @param str $username Twitter handle/username
+   * @param array $opts Array of options
+   * @return void
+   */
+  public function __construct($username='', $opts=array()){
+    $this->set_username($username);
+    $default_opts = array(
+      'exclude_replies' => false,
+      'limit' => 5,
+      'parse_links' => true,
+      'link_class' => 'parsed-link',
+      'profanity_filter' => false,
+      'profanity_filter_method' => 'censor',
+      'profanity_blacklist' => null,
+      'profanity_blacklist_overwrite' => false,
+      'profanity_whitelist' => false,
+      'use_cache' => false,
+      'cache_filename' => sprintf('tweetcache-%s.json', $this->get_username()),
+      'cache_path' => dirname(__FILE__)
+    );
+    $this->opts = array_merge($default_opts, $opts);
+    return;
+  }
+
+  /**
+   * Set an option in the $opts property
+   * @param str $key The key of the $opts array to set
+   * @param mixed $val The value to set for $key
+   * @return void
+   */
+  public function set_opt($key, $val=''){
+    $key = $this->clean_key($key);
+    $this->opts[$key] = $val;
+    return;
+  }
+
+  /**
+   * Get an value from the $opts property
+   * @param str $key The key to retrieve
+   * @param mixed $default The value to return if $key isn't set
+   * @return mixed
+   */
+  public function get_opt($key, $default=false){
+    $key = $this->clean_key($key);
+    return ( isset($this->opts[$key]) ? $this->opts[$key] : $default );
+  }
+
+  /**
+   * Set the $username class property
+   * @param str $user The username to set
+   * @return void
+   */
+  public function set_username($user){
+    $user = trim(filter_var($user, FILTER_SANITIZE_STRING));
+    if( intval($user) > 0 ){
+      trigger_error('SimpleTwitterTimeline does not accept user IDs (yet)', E_USER_WARNING);
+      return false;
+    } else if( $user !== '' ){
+      $this->username = $user;
+      return true;
+    } else {
+      trigger_error('Invalid username', E_USER_WARNING);
+      return false;
+    }
+  }
+
+  /**
+   * Get the username property
+   * @return str
+   */
+  public function get_username(){
+    return $this->username;
+  }
+
+  /**
+   * Get a link to $this->username's profile
+   * @return str
+   */
+  public function get_profile_url(){
+    return sprintf('https://twitter.com/#!/%s', $this->get_username());
+  }
+
+  /**
+   * Generate an HTML link to $this->username's profile
+   * The link will be in the format of <a href="{url}" title="Follow @{user} on Twitter" rel="external">@{user}</a>
+   * @return str
+   */
+  public function get_profile_link(){
+    $user = $this->get_username();
+    return '<a href="' . $this->get_profile_url() . '" title="' . sprintf('Follow @%s on Twitter', $user) . '" rel="external">@' . $user . '</a>';
+  }
+
+  /**
+   * Sanitize a key and make sure it only contains alphanumeric characters, dashes, and/or underscores
+   * @param str $key
+   * @return str
+   */
+  public function clean_key($key){
+    return preg_replace('/[^A-Za-z0-9-_]/i', '', $key);
+  }
+
+  /**
+   * Helper function: convert a comma-separated list into an array. Will leave arrays untouched except for filtering empty values
+   * @param mixed $list
+   * @return array
+   */
+  public function create_array($list){
+    if( !is_array($list) ){
+      if( is_string($list) ){
+        $list = array_map('trim', explode(',', $list));
+      } else {
+        trigger_error(sprintf('Unable to convert %s to array', gettype($list)));
+      }
+    }
+    return array_filter($list);
+  }
+
+  /**
+   * Get a user's public timeline
+   * @return object
+   */
+  public function get_timeline(){
+
+    // Determine if we're using the cached version or making an API call
+    if( !$this->get_opt('use_cache') || $this->cache_valid() ){
+      $tweets = $this->get_public_timeline();
+
+      // TODO: Apply filters
+
+      // Save the cache
+      if( $this->get_opt('use_cache') ){
+        $this->save_tweet_cache($tweets);
+      }
+
+    // Use the cache
+    } else {
+      $tweets = $this->get_tweet_cache();
+    }
+    return $tweets;
+  }
+
+  /**
+   * Fetch a fresh copy from the Twitter API
+   * @return array
+   */
+  public function get_public_timeline(){
+    $feed = new stdClass;
+    $url = sprintf('https://api.twitter.com/1/statuses/user_timeline.json?screen_name=%s', $this->get_username());
+    $url .= sprintf('&count=%d', $this->get_opt('limit'));
+    $url .= sprintf('&exclude_replies=%d',$this->get_opt('exclude_replies'));
+
+    if( $json = file_get_contents($url) ){
+      $feed = json_decode($json, false);
+    }
+    return $feed;
+  }
+
+  /**
+   * Retrieve the tweet cache
+   * @return mixed
+   */
+  public function get_tweet_cache(){
+    $file = sprintf('%s/%s', $this->get_opt('cache_path'), $this->get_opt('cache_filename'));
+    if( file_exists($file) && $cache = file_get_contents($file) ){
+      return json_decode($cache, false);
+    } else {
+      return false;
+    }
+  }
+
+  /**
+   * Save a cache of the public timeline
+   * @param object The twitter object to save
+   * @return bool
+   */
+  public function save_tweet_cache($tweets){
+    $file = sprintf('%s/%s', $this->get_opt('cache_path'), $this->get_opt('cache_filename'));
+    if( $fh = fopen($file, 'a') ){
+      fwrite($fh, json_encode($tweets));
+      fclose($fh);
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  /**
+   * Determine whether or not the tweet cache is valid
+   * @return bool
+   */
+  public function cache_valid(){
+    // TODO: Apply some logic here
+    return true;
+  }
+
+/** Tweet filters */
+
+  /**
+   * Parse a tweet and activate any links
+   * @param str $tweet The tweet's content
+   * @return str
+   */
+  public function parse_links($tweet){
+    preg_match_all('/(http(?:s?):\/\/\S+)/i', $tweet, $links);
+    if( isset($links['1']) ){
+      foreach( $links['1'] as $url ){
+        // Only proceed if PHP's built-in filter sees it as a URL
+        if( filter_var($url, FILTER_VALIDATE_URL, FILTER_FLAG_HOST_REQUIRED) ){
+          $tweet = str_replace($url, sprintf('<a href="%s" class="%s">%s</a>', $url, $this->get_opt('link_class'), $url), $tweet);
+        }
+      }
+    }
+    return $tweet;
+  }
+
+  /**
+   * Profanity filter
+   * @param str $tweet The tweet's content
+   * @return str|bool If $this->opts['filter_method'] == 'skip' the profane tweets will be skipped and will return false
+   */
+  public function profanity_filter($tweet){
+    $default_blacklist = array('shit', 'piss', 'cunt', 'fuck', 'cocksucker', 'motherfucker', 'tits');
+    $default_whitelist = array('shitake', 'pissed');
+    $words = array();
+
+    foreach( array('blacklist', 'whitelist') as $key ){
+      $default_list = sprintf('default_%s', $key);
+      $user_list = sprintf('user_%s', $key);
+      $$user_list = ( $this->get_opt(sprintf('profanity_%s', $key), false) ? $this->create_array($this->get_opt(sprintf('profanity_%s', $key), false)) : array() );
+      $$key = array_map('strtolower', ( $this->get_opt(sprintf('profanity_%s_overwrite', $key), false) ? $$user_list : array_merge($$default_list, $$user_list) ));
+    }
+
+    foreach( $blacklist as $word ){
+      if( preg_match_all('/\S*(' . $word . ')\S*/i', $tweet, $matches) ){
+        foreach( $matches['1'] as $k=>$v ){
+          if( !in_array(strtolower($matches['0'][$k]), $user_whitelist) && !in_array(strtolower($matches['1'][$k]), $user_whitelist) && !filter_var($matches['0'][$k], FILTER_VALIDATE_URL) ){
+            $words[] = $v;
+          }
+        }
+      }
+    }
+
+    // Skip offending tweets
+    if( strtolower($this->get_opt('profanity_filter_method')) === 'skip' ){
+      if( !empty($words) ){ // We have at least one match
+        $tweet = false;
+      }
+
+    // Censor the tweet
+    } else {
+      foreach( $words as $word ){
+        $tweet = str_replace($word, str_repeat('*', strlen($word)), $tweet);
+      }
+    }
+
+    return $tweet;
+  }
+}
